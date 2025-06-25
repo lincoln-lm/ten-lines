202247c97268be1f2d3ef97f748ddf6dbf7c1ab9
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2025-06-25 03:59:47 -0600

    dont access mode in defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
     assetsInclude: ["**/*.bin"],
     plugins: [
         react(),
