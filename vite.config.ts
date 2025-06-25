80766857bd4569f42bce9d8a28bc2f6b4ed375fd
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2025-06-25 01:44:35 -0600

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
     assetsInclude: ["**/*.bin"],
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            workbox: {
+                maximumFileSizeToCacheInBytes: 5000000,
+            },
+            devOptions: {
+                enabled: true,
+            },
+        }),
+    ],
     build: {
-        minify: false,
+        minify: true,
     },
     base: "/ten-lines/",
 });
