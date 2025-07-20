46a12208d9d150a262a1f72b6743e3ac3d18e69a
lincoln-lm <73306575+lincoln-lm@users.noreply.github.com>
2025-07-19 22:15:05 -0600

    stop offsetting LG ENG by 45

--- src/wasm/generate_ten_lines_precalc.py
+++ src/wasm/generate_ten_lines_precalc.py
@@ -99,7 +99,7 @@ def pull_frlg_seeds():
         timeout=15,
     ).text
     sheet_csv = csv.reader(sheet_txt.split("\n"))
-    lg_eng_seeds = SeedDataStore(starting_frame=4062 // 2 - 45, frame_size=1)
+    lg_eng_seeds = SeedDataStore(starting_frame=4062 // 2, frame_size=1)
     for i, row in enumerate(sheet_csv):
         if i == 0:
             continue
